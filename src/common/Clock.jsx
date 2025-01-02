8c901fce3839abff51a26972c4fed1d67eb49e07
Maxim Nizamov <lucors.dev@gmail.com>
2025-01-02 20:44:36 +0400

    shit, i forget initial state

--- src/common/Clock.jsx
+++ src/common/Clock.jsx
@@ -3,14 +3,14 @@ import "./Clock.css";
 
 export default function Clock() { 
   const now = new Date();
-  const [time, setTime] = useState(now.toLocaleTimeString());
-  const [date, setDate] = useState(now.toLocaleDateString());
+  const [time, setTime] = useState(now.toLocaleTimeString("ru-RU"));
+  const [date, setDate] = useState(now.toLocaleTimeString("ru-RU"));
 
   useEffect(() => {
     const interval = setInterval(function(){
       const now = new Date();
       setTime(now.toLocaleTimeString("ru-RU"));
-      setDate(now.toLocaleDateString("ru-RU"));
+      setDate(now.toLocaleTimeString("ru-RU"));
     }, 1000);
 
     return () => {
