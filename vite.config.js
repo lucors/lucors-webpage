4a8657f42bb4d8cc38a0c6d2481b068cb27230fa
Maxim Nizamov <lucors.dev@gmail.com>
2025-11-01 12:42:36 +0400

    Добавил поддержку удаленных компонентов

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {federation} from '@module-federation/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'host',
+      remotes: {
+        remote_apps: {
+          type: 'module',
+          entry: 'http://localhost:3000/remoteEntry.js'
+        }
+      },
+      shared: {
+        react: { singleton: true, strictVersion: true },
+        'react-dom': { singleton: true, strictVersion: true },
+        'react-i18next': { singleton: true, strictVersion: true },
+        i18next: { singleton: true, strictVersion: true }
+      }
+    })
+  ],
   build: {
+    target: 'esnext',
     rollupOptions: {
       output: {
         assetFileNames: '[name]-[hash][extname]',
